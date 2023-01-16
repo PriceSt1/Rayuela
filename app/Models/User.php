cfc84bed0600bbb2d845f37a5f62620c94745d50
Felipe Alejandro Borjas de los Dolores <falejandro.borjas@gmail.com>
2023-01-16 11:32:58 +0000

    env file created

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'idUsuario',
         'name',
         'email',
         'password',
+        'idRol'
     ];
 
     /**
